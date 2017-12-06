6e79aef473d3b0c214a953c317029900e7e68339
leonardo miraglia <leo.mi.me@gmail.com>
2017-12-06 02:32:28 -0200

    change Props from button and margin

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './button.css'
 
-const Button = () => (
+const Button = ({text}) => (
     <div className='btn'>
-        Shop Now
+        {text}
     </div>
 )
 

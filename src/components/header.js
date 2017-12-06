ab51da0dd59021a44acda29381acd0bbdfd266ff
leonardo miraglia <leo.mi.me@gmail.com>
2017-12-06 02:33:06 -0200

    Logo adding on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './header.css'
-import MdShoppingCart from 'react-icons/lib/md/shopping-cart';
+import MdShoppingCart from 'react-icons/lib/md/shopping-cart'
+import {Link} from 'react-router-dom'
 const Header = () => (
     <header>
+        <Link to={'/'}><p className='logo'>Amaro</p></Link>
         <MdShoppingCart className='icon'/>
     </header>    
 )
